1cc24275489d6b3fbd1551f9b0b67cd656377831
ENZO FELIX CARRILHO <25132408@CFP127ALUNO.LOCAL>
2025-09-24 08:57:31 -0300

    estruturação básica do arquivo de funções

---
+++ modulo/funcoes.js
@@ -0,0 +1,34 @@
+/***********************************************************************************************************************
+ * Objetivo: Arquivo de funções para gerenciar a API do projeto Whatsapp
+ * Data: 24/09/2025
+ * Autor: Enzo Felix Carrilho
+ * Versão: 1.0
+ * ***********************************************************************************************************************/ 
+
+const dados = require('./contatos.js')
+const MESSAGE_ERROR = {status: false, status_code: 500, development: 'Enzo Felix Carrilho'}
+
+const getAllUsers = function(){
+}
+
+const getUserProfile = function(userId){
+
+}
+
+const getUserContacts = function(userId){
+
+}
+
+const getUserMessages = function(userId){
+
+}
+
+const getMessagesByContact = function(Contact){
+
+}
+
+const getMessageByKeyWord = function(Contact, KeyWord){
+
+}
+
+getAllUsers()
